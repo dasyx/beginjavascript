4c5c8af7d10339f1cdeae8196354889b02df80ba
unknown <seanpnn00@gmail.com>
2023-09-08 19:10:23 +0200

    work on error handle in strings

--- script.js
+++ script.js
@@ -2,18 +2,34 @@
 import { prompt } from "./helper.js";
 
 let mail = await prompt("Enter recipient your mail : ");
-let object = await prompt("Enter your object : ");
-let message = await prompt("Enter your message : ");
+let object = "";
+let message = "";
 
-console.log("NODE-MAILER ™️");
+if (mail.length === 0) {
+  console.log("Error : You must enter a mail, an object and a message");
+  process.exit(1);
+} else {
+  object = await prompt("Enter your object : ");
+  if (object.length === 0) {
+    console.log("Error : You must enter an object");
+    process.exit(1);
+  } else {
+    message = await prompt("Enter your message : ");
+    if (message.length === 0) {
+      console.log("Error : You must enter a message");
+      process.exit(1);
+    } else {
+      console.log("NODE-MAILER ™️");
 
-console.log("Envoi du mail en cours...");
-
-console.log(`To user : ${mail}
+      console.log(`To user : ${mail}
 Object : ${object}
 Message : ${message}
 `);
 
+      console.log("Envoi du mail en cours...");
+    }
+  }
+}
 // 🦁 Récupère le mail, l'objet et le message
 
 // 🦁 Affiche un log
